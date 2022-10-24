c5eb3abc9e490b54df0f20fc72c773637c38fe8f
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-10-24 10:41:58 +0900

    ユーザー登録時にwebシステムの登録画面に遷移させる処理を追加。

---
+++ Assets/Scripts/RegisterController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+       public void onClick()
+    {
+        Application.OpenURL("http://localhost:8000/register");//""の中には開きたいWebページのURLを入力します
+    }
+}
